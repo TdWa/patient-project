1ba2b8e990af48b3c7458157236a34bf4cbbbf11
TdWa <59452572+TdWa@users.noreply.github.com>
2020-10-08 17:52:55 +0200

    changed port and added gitignore node_modules/

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ app.get(route, (req, res) => {
   res.send(render(patient))
 })
 
-const port =  process.env.PORT || 3000
+const port = 3000
 app.listen(port, () => console.log(`listening to port: ${port}`))
 
 
--- main.js
+++ main.js
@@ -2,6 +2,6 @@ document.getElementById("patientID").addEventListener("change", changePatientID)
 
 function changePatientID () {
   const id = document.getElementById("patientID").value
-  document.getElementById("link").setAttribute("href", `http://localhost:4000/patient/${id}`);
+  document.getElementById("link").setAttribute("href", `http://localhost:3000/patient/${id}`);
 }
 
